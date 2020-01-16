4a9013359d60a586eb05897de0c8f11626e1d9fe
aait <info@aait.se>
2020-01-16 15:48:38 +0600

    Update tests

--- src/PayEx/Framework/DataObjectCollectionItem.php
+++ src/PayEx/Framework/DataObjectCollectionItem.php
@@ -4,6 +4,10 @@ namespace PayEx\Framework;
 
 use PayEx\Framework\Data\DataObjectCollectionItemInterface;
 
+/**
+ * Class DataObjectCollection
+ * @SuppressWarnings(PHPMD.NumberOfChildren)
+ */
 class DataObjectCollectionItem extends AbstractSimpleDataObject implements DataObjectCollectionItemInterface
 {
 }
