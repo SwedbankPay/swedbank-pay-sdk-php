ae448bc97f744034ad62cffcc83c5825d553b787
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2020-04-04 00:35:55 +0200

    Look for version in composer.lock as well.

--- src/SwedbankPay/Api/Client/Version.php
+++ src/SwedbankPay/Api/Client/Version.php
@@ -50,14 +50,41 @@ class Version
             return getenv("VERSION");
         }
 
-        $path = __DIR__ . '/../../../../composer.json';
-        $contents = file_get_contents($path);
-        $data = json_decode($contents, true);
+        $composer = $this->readJson(__DIR__ . '/../../../../composer.json');
 
-        if (isset($data['version'])) {
-            return $data['version'];
+        if (isset($composer['version'])) {
+            return $composer['version'];
+        }
+
+        $composerLock = $this->readJson(__DIR__ . '/../../../../composer.lock');
+
+        if (isset($composerLock['packages'])) {
+            $packages = $composerLock['packages'];
+            foreach ($packages as $package) {
+                if (!isset($package['name']) ||
+                    $package['name'] != "swedbank-pay/swedbank-pay-sdk-php") {
+                    continue;
+                }
+
+                if (isset($package['version'])) {
+                    return $package['version'];
+                }
+            }
         }
 
         throw new ClientException('VERSION not found in environment variable, composer.json or anywhere else.');
+    }
+
+
+    /**
+     * Reads the file at $path and returns it as a JSON decoded object.
+     *
+     * @param $path The path of the file to read and JSON decode.
+     * @return object The JSON decoded object.
+     */
+    private function readJson($path)
+    {
+        $contents = file_get_contents($path);
+        return json_decode($contents, true);
     }
 }
