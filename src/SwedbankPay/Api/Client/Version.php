97b60f95b50ba792cc3d28f855d1053572aebf42
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2020-04-05 00:19:26 +0200

    Look for composer.lock in the app-relative directory

--- src/SwedbankPay/Api/Client/Version.php
+++ src/SwedbankPay/Api/Client/Version.php
@@ -50,13 +50,15 @@ class Version
             return getenv("VERSION");
         }
 
-        $composer = $this->readJson(__DIR__ . '/../../../../composer.json');
+        $composerPath = __DIR__ . '/../../../../composer.json';
+        $composer = $this->readJson($composerPath);
 
         if (isset($composer['version'])) {
             return $composer['version'];
         }
 
-        $composerLock = $this->readJson(__DIR__ . '/../../../../composer.lock');
+        $composerLockPath = getcwd() . DIRECTORY_SEPARATOR . 'composer.lock';
+        $composerLock = $this->readJson($composerLockPath);
 
         if (isset($composerLock['packages'])) {
             $packages = $composerLock['packages'];
@@ -97,29 +99,43 @@ class Version
     * @param string The original path, can be relative etc.
     * @return string The resolved path, it might not exist.
     */
-    private function truePath($path){
+    private function truePath($path)
+    {
         // whether $path is unix or not
-        $unipath=strlen($path)==0 || $path{0}!='/';
+        $unipath = strlen($path) == 0 || $path{0} != '/';
+
         // attempts to detect if path is relative in which case, add cwd
-        if(strpos($path,':')===false && $unipath)
-            $path=getcwd().DIRECTORY_SEPARATOR.$path;
+        if (strpos($path, ':') === false && $unipath) {
+            $path = getcwd() . DIRECTORY_SEPARATOR . $path;
+        }
+
         // resolve path parts (single dot, double dot and double delimiters)
         $path = str_replace(array('/', '\\'), DIRECTORY_SEPARATOR, $path);
         $parts = array_filter(explode(DIRECTORY_SEPARATOR, $path), 'strlen');
         $absolutes = array();
+
         foreach ($parts as $part) {
-            if ('.'  == $part) continue;
+            if ('.'  == $part) {
+                continue;
+            }
+
             if ('..' == $part) {
                 array_pop($absolutes);
             } else {
                 $absolutes[] = $part;
             }
         }
-        $path=implode(DIRECTORY_SEPARATOR, $absolutes);
+
+        $path = implode(DIRECTORY_SEPARATOR, $absolutes);
+
         // resolve any symlinks
-        if(file_exists($path) && linkinfo($path)>0)$path=readlink($path);
+        if (file_exists($path) && linkinfo($path) > 0) {
+            $path = readlink($path);
+        }
+
         // put initial separator that could have been lost
-        $path=!$unipath ? '/'.$path : $path;
+        $path = !$unipath ? '/' . $path : $path;
+
         return $path;
     }
 }
