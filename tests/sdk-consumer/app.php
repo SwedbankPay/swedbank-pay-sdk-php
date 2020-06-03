84141d058cf76c815b37f7534df159a3652bf46a
aait <info@aait.se>
2020-06-03 16:59:34 +0600

    Update docker

--- tests/sdk-consumer/app.php
+++ tests/sdk-consumer/app.php
@@ -6,6 +6,11 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use \SwedbankPay\Api\Client;
 
+$version = getenv('VERSION');
+if (!empty($version)) {
+    define('SwedbankPay\\Api\\Client\\VERSION', $version);
+}
+
 $client = new \SwedbankPay\Api\Client\Client();
 $clientVersion = new \SwedbankPay\Api\Client\ClientVersion();
 $versionNumber = $clientVersion->getVersion();
@@ -13,5 +18,3 @@ $versionNumber = $clientVersion->getVersion();
 // phpcs:disable
 echo "sdkversion: $versionNumber";
 // phpcs:enable
-
-?>
