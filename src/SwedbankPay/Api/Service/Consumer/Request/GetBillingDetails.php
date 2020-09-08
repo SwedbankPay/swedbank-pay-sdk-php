27925d84daf8360342aecd3c3c4c6daf67680670
aait <info@aait.se>
2020-09-08 15:38:26 +0600

    Updates

--- src/SwedbankPay/Api/Service/Consumer/Request/GetBillingDetails.php
+++ src/SwedbankPay/Api/Service/Consumer/Request/GetBillingDetails.php
@@ -6,9 +6,32 @@ use SwedbankPay\Api\Service\Request;
 
 class GetBillingDetails extends Request
 {
+    const BILLING_DETAILS_URI = 'billing_details_uri';
+
     public function setup()
     {
         $this->setRequestMethod('GET');
-        $this->setRequestEndpoint('/psp/consumers/%s/billing-details');
+        $this->setRequestEndpoint($this->getBillingDetailsUri());
+    }
+
+    /**
+     * Set Billing Details Uri.
+     *
+     * @param string $uri
+     * @return $this
+     */
+    public function setBillingDetailsUri($uri)
+    {
+        return $this->offsetSet(self::BILLING_DETAILS_URI, $uri);
+    }
+
+    /**
+     * Get Billing Details Uri.
+     *
+     * @return string|null
+     */
+    public function getBillingDetailsUri()
+    {
+        return $this->offsetGet(self::BILLING_DETAILS_URI);
     }
 }
--- src/SwedbankPay/Api/Service/Creditcard/Request/RemovePaymentToken.php
+++ src/SwedbankPay/Api/Service/Creditcard/Request/RemovePaymentToken.php
@@ -6,9 +6,32 @@ use SwedbankPay\Api\Service\Request;
 
 class RemovePaymentToken extends Request
 {
+    const INSTRUMENT_DATA_URI = 'request_method';
+
     public function setup()
     {
         $this->setRequestMethod('PATCH');
-        $this->setRequestEndpoint('/psp/creditcard/payments/instrumentData/%s');
+        $this->setRequestEndpoint($this->getInstrumentDataUri());
+    }
+
+    /**
+     * Set Instrument Data Uri.
+     *
+     * @param string $uri
+     * @return RemovePaymentToken
+     */
+    public function setInstrumentDataUri($uri)
+    {
+        return $this->offsetSet(self::INSTRUMENT_DATA_URI, $uri);
+    }
+
+    /**
+     * Get Instrument Data Uri.
+     *
+     * @return string|null
+     */
+    public function getInstrumentDataUri()
+    {
+        return $this->offsetGet(self::INSTRUMENT_DATA_URI);
     }
 }
