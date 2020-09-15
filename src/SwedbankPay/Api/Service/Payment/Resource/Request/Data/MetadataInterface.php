65885ce31832d9e62974a76b5b3c9a60784a6b93
aait <info@aait.se>
2020-09-15 14:06:26 +0600

    Update Metadata class

--- src/SwedbankPay/Api/Service/Payment/Resource/Request/Data/MetadataInterface.php
+++ src/SwedbankPay/Api/Service/Payment/Resource/Request/Data/MetadataInterface.php
@@ -11,10 +11,6 @@ use SwedbankPay\Api\Service\Data\ResourceInterface;
  */
 interface MetadataInterface extends ResourceInterface
 {
-    const KEY1 = 'key1';
-    const KEY2 = 'key2';
-    const KEY3 = 'key3';
-    const KEY4 = 'key4';
 
     /**
      * Get Data.
@@ -34,46 +30,11 @@ interface MetadataInterface extends ResourceInterface
     public function setData($key, $value);
 
     /**
-     * @return string
-     */
-    public function getKey1();
-
-    /**
-     * @param string $key1
+     * Unset Data.
+     *
+     * @param string $key
+     * @param mixed $value
      * @return $this
      */
-    public function setKey1($key1);
-
-    /**
-     * @return int
-     */
-    public function getKey2();
-
-    /**
-     * @param int $key2
-     * @return $this
-     */
-    public function setKey2($key2);
-
-    /**
-     * @return float
-     */
-    public function getKey3();
-
-    /**
-     * @param float $key3
-     * @return $this
-     */
-    public function setKey3($key3);
-
-    /**
-     * @return mixed
-     */
-    public function getKey4();
-
-    /**
-     * @param mixed $key4
-     * @return $this
-     */
-    public function setKey4($key4);
+    public function unsData($key);
 }
--- src/SwedbankPay/Api/Service/Payment/Resource/Request/Metadata.php
+++ src/SwedbankPay/Api/Service/Payment/Resource/Request/Metadata.php
@@ -34,70 +34,14 @@ class Metadata extends Resource implements MetadataInterface
     }
 
     /**
-     * @return string
-     */
-    public function getKey1()
-    {
-        return $this->offsetGet(self::KEY1);
-    }
-
-    /**
-     * @param string $key1
+     * Unset Data.
+     *
+     * @param string $key
+     * @param mixed $value
      * @return $this
      */
-    public function setKey1($key1)
+    public function unsData($key)
     {
-        return $this->offsetSet(self::KEY1, $key1);
-    }
-
-    /**
-     * @return int
-     */
-    public function getKey2()
-    {
-        return $this->offsetGet(self::KEY2);
-    }
-
-    /**
-     * @param int $key2
-     * @return $this
-     */
-    public function setKey2($key2)
-    {
-        return $this->offsetSet(self::KEY2, $key2);
-    }
-
-    /**
-     * @return float
-     */
-    public function getKey3()
-    {
-        return $this->offsetGet(self::KEY3);
-    }
-
-    /**
-     * @param float $key3
-     * @return $this
-     */
-    public function setKey3($key3)
-    {
-        return $this->offsetSet(self::KEY3, $key3);
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getKey4()
-    {
-        return $this->offsetGet(self::KEY4);
-    }
-
-    /**
-     * @param mixed $key4
-     * @return $this
-     */
-    public function setKey4($key4)
-    {
-        return $this->offsetSet(self::KEY4, $key4);
+        return $this->offsetUnset($key);
     }
 }
